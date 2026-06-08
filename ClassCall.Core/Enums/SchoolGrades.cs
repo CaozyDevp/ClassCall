88e28ce2cdbeaf109de9d2baa232f6e9c5f96f57
Zachary Cao <caozydev@qq.com>
2026-06-08 10:19:28 +0800

    feat: add descriptions to SchoolGrade enum

--- ClassCall.Core/Enums/SchoolGrades.cs
+++ ClassCall.Core/Enums/SchoolGrades.cs
@@ -1,12 +1,20 @@
-﻿namespace ClassCall.Core.Enums
+﻿using System.ComponentModel;
+
+namespace ClassCall.Core.Enums
 {
     public enum SchoolGrades : byte
     {
+        [Description("初一")]
         Junior1 = 11,
+        [Description("初二")]
         Junior2 = 12,
+        [Description("初三")]
         Junior3 = 13,
+        [Description("高一")]
         Senior1 = 14,
+        [Description("高二")]
         Senior2 = 15,
+        [Description("高三")]
         Senior3 = 16,
     }
 }
