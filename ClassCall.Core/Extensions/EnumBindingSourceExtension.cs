9e455cddea5914287fe375d4ba673f2ef31042d1
Zachary Cao <caozydev@qq.com>
2026-06-08 11:12:42 +0800

    feat: add EnumBindingSourceExtension

---
+++ ClassCall.Core/Extensions/EnumBindingSourceExtension.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace ClassCall.Core.Extensions
+{
+    /// <summary>
+    /// 这个类参考了
+    /// https://brianlagunas.com/how-to-bind-an-enum-to-a-combobox-in-wpf/
+    /// </summary>
+    public class EnumBindingSourceExtension : MarkupExtension
+    {
+        private Type _enumType;
+
+        public Type EnumType
+        {
+            get => _enumType;
+            set
+            {
+                if (value != _enumType)
+                {
+                    if (value != null)
+                    {
+                        var enumType = Nullable.GetUnderlyingType(value) ?? value;
+                        if (!enumType.IsEnum)
+                            throw new ArgumentException("The type must be an Enum.");
+                    }
+                    _enumType = value;
+                }
+            }
+        }
+
+        public EnumBindingSourceExtension()
+        {
+            
+        }
+
+        public EnumBindingSourceExtension(Type enumType)
+        {
+            EnumType = enumType;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if(_enumType == null)
+                throw new InvalidOperationException("The EnumType must be specified.");
+
+            var actualEnumType = Nullable.GetUnderlyingType(_enumType) ?? _enumType;
+            var enumValues = Enum.GetValues(actualEnumType);
+
+            if(actualEnumType == _enumType)
+                return enumValues;
+
+            var tempArray = Array.CreateInstance(actualEnumType, enumValues.Length + 1);
+            enumValues.CopyTo(tempArray, 1);
+            return tempArray;
+        }
+    }
+}
