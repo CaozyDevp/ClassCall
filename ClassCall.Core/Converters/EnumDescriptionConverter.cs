e178bbc4df83c74202d8ad5c808ac40b9f8b5cfb
Zachary Cao <caozydev@qq.com>
2026-06-08 10:44:33 +0800

    feat: add EnumDescriptionConverter

---
+++ ClassCall.Core/Converters/EnumDescriptionConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ClassCall.Core.Converters
+{
+    public class EnumDescriptionConverter : IValueConverter
+    {
+        /// <summary>
+        /// 获取枚举类型的Description
+        /// </summary>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return DependencyProperty.UnsetValue;
+            return GetDescription(value);
+        }
+
+        /// <summary>
+        /// 该方法尚未实现
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        private object GetDescription(object value)
+        {
+            var field = value.GetType().GetField(value.ToString());
+            var attributes = field.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            if(attributes == null || attributes.Length == 0)
+            {
+                return attributes[0];
+            }
+            return value.ToString();
+        }
+    }
+}
